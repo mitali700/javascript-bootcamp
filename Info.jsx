98520e8ecc83f400e54045b5ca2cd23fa9f91e3a
mitali700 <84939615+mitali700@users.noreply.github.com>
2021-06-27 11:53:12 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>7-day Javascript & React bootcamp</h1>
+      <p>All files and codes are submitted by Mitali Nigam</p>
+    </div>
+  );
+}
+
+export default Info;
